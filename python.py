aff66af9973e7fa7be30c283f0113c3aa492a095
dev-journey <jasicaaggarwal50@gmail.com>
2026-06-18 14:58:11 +0530

    python basics practise

---
+++ python.py
@@ -0,0 +1,27 @@
+#variables and data types
+name="Jasica Aggarwal"
+age=19
+cgpa=7
+is_student=True
+print("Name:",name)
+print("Age:",age)
+print("CGPA:",cgpa)
+print("Is Student:",is_student)
+
+#strings
+first_name="Jasica"
+last_name="Aggarwal"
+full_name=first_name+" "+last_name
+print("Full Name:",full_name)
+print("Length of Full Name:",len(full_name))
+print("Uppercase Full Name:",full_name.upper())
+
+#conditional statements
+marks=85
+if marks>=90:
+    print("Grade: A")
+elif marks>=80:
+    print("Grade: B")
+elif marks>=70:
+    print("Grade: C")
+else:    print("Grade: F")
