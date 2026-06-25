d5b259be3ba4889bd05ebf4f718ad5cc75b02790
dev-journey <jasicaaggarwal50@gmail.com>
2026-06-25 21:06:38 +0530

    learnt functions and other regarding python

--- day2.py
+++ day2.py
@@ -1,4 +1,21 @@
-#loops 
+#loops
+# for loop
+for i in range(5):
+    print("Iteration:",i)
+
+    #while loop
+count=0
+while count<5:
+    print("Count:",count)
+    count+=1
+
+#do-while loop
+count=0
+while True:
+    print("Count:",count)
+    count+=1
+    if count>=5:
+        break
 
 
 
