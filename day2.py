b34e263b185a9ce6848ee6476a657adf1bf844ae
dev-journey <jasicaaggarwal50@gmail.com>
2026-06-26 20:13:28 +0530

    recursion

--- day2.py
+++ day2.py
@@ -17,9 +17,18 @@ while True:
     if count>=5:
         break
 
+#conditional statements and loops together
+for i in range(10):
+    if i%2==0:
+        print(i,"is even")
+    else:
+        print(i,"is odd")
 
+#functions
+def greet(name):
+    return "Hello, "+name+"!"
+print(greet("Alice"))
+print(greet("Bob"))
 
+#recursion
 
-
-
-# # functions
