165448f2bcf72fe2c0f5e0845957a72516be0f07
dev-journey <jasicaaggarwal50@gmail.com>
2026-06-29 19:56:25 +0530

    Updated day2.py

--- day2.py
+++ day2.py
@@ -31,4 +31,23 @@ print(greet("Alice"))
 print(greet("Bob"))
 
 #recursion
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5))
 
+def sum_numbers(n):
+    if n == 1:
+        return 1
+    return n + sum_numbers(n-1)
+
+print(sum_numbers(5))
+
+def reverse_string(s):
+    if len(s) == 0:
+        return s
+    return reverse_string(s[1:]) + s[0]
+
+print(reverse_string("Python"))
